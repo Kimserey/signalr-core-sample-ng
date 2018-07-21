344f2c43574c268c2e5d9bfb53bb30e1897eceb8
Kimserey Lam <kimserey.lam@groomgy.com>
2018-07-21 14:38:55 +0100

    Added aspnet signalr and notification.service

---
+++ src/app/notification.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import * as signalR from "@aspnet/signalr";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class NotificationService {
+  private connection: signalR.HubConnection;
+
+  connect(accessToken) {
+    if (!this.connection) {
+      this.connection = new signalR.HubConnectionBuilder()
+        .withUrl("/hubs/messages", { accessTokenFactory: () => accessToken })
+        .build();
+        
+      this.connection.on("receive", (user, msg) => {
+        console.log('do something')
+      });
+      
+      this.connection.start().catch(err => console.error(err));
+    }
+  }
+
+  disconnect() {
+    if (this.connection) {
+      this.connection.stop();
+      this.connection = null;
+    }
+  }
+}
